0111e182066e98d5f47d1fe9c5aa208eefcb1a82
Sudhan Shiva <sudhanshiva2004@gmail.com>
2025-02-22 23:46:02 +0530

    screen shots

---
+++ BoilerController/BoilerController/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    static void Main()
+    {
+        BoilerTemperatureReader tempReader = new BoilerTemperatureReader();
+        tempReader.GetBoilerTemperature();
+        tempReader.GetBoilerTemperature();
+        tempReader.GetBoilerTemperature();
+        tempReader.GetBoilerTemperature();
+    }
+
+    
+}
+
+public class BoilerTemperatureReader
+{
+    public delegate TempNotifier(double temperature);
+    public event TempNotifier();
+
+    public double ReadTemperature()
+    {
+        double minTemp = 80.0;
+        double maxTemp = 120.0;
+        double temperature = minTemp + new Random().NextDouble() * (maxTemp - minTemp);
+
+
+        return temperature;
+    }
+
+    public void TempNotifier()
+    {
+    }
+}
+
+public class PhoneReader
+{
+    public void DisplayAlert(double currentTemp)
+    {
+        if(currentTemp > 110)
+        Console.WriteLine($"{currentTemp}---Alert in phone");
+    }
+}
+
+public class LaptopReader
+{
+    public void DisplayAlert(double currentTemp)
+    {
+        if(currentTemp > 100)
+        Console.WriteLine($"{currentTemp}---Alert in laptop");
+    }
+}
